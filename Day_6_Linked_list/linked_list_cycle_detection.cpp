26a04f248ac2b049eb26f11dc17cbb132704a608
Pranav N <pranavbharadwaj142@gmail.com>
2022-09-08 20:39:31 +0530

    linked-list-cycle-detection

---
+++ Day_6_Linked_list/linked_list_cycle_detection.cpp
@@ -0,0 +1,19 @@
+
+// Link : https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow)return true;
+            
+        }
+        return false;
+    }
+};
