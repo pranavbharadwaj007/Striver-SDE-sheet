98aa31a2fa4d69e920f270ce09f06976b5b7d5ab
Pranav N <pranavbharadwaj142@gmail.com>
2022-10-16 14:12:02 +0530

    Permutation Sequence

---
+++ Day_9_Recursion/permutation_sequence.cpp
@@ -0,0 +1,24 @@
+// Link : https://leetcode.com/problems/permutation-sequence/
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact=1;
+        vector<int>num;
+        for(int i=1;i<n;i++){
+            fact=fact*i;
+            num.push_back(i);
+        }
+        num.push_back(n);
+        string tem="";
+        k--;
+        while(true){
+            tem+=to_string(num[k/fact]);
+            num.erase(num.begin()+(k/fact));
+            if(num.size()==0)break;
+            k=k%fact;
+            fact=fact/num.size();
+        }
+        return tem;
+    }
+};
