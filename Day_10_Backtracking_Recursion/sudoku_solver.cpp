46204d1cfdbee991b2e502cedf553f4b2dd3e432
Pranav N <pranavbharadwaj142@gmail.com>
2022-10-19 20:37:35 +0530

    Sudoku Solver

---
+++ Day_10_Backtracking_Recursion/sudoku_solver.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isp(vector<vector<char>>& board,int row,int col, char ch){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==ch)return false;
+            
+            if(board[i][col]==ch)return false;
+            
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch)return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j]=='.'){
+                for(char ch='1';ch<='9';ch++){
+                 if(isp(board,i,j,ch)){
+                    board[i][j]=ch;
+                     if(solve(board)){
+                      return true;   
+                     }
+                     board[i][j]='.';
+                   }                   
+                }  
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+        
+    }
+};
