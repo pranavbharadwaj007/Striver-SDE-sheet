3c8e8f4da005bb336c5aa50d9aca66edf71a6db6
Pranav N <pranavbharadwaj142@gmail.com>
2022-11-10 22:11:25 +0530

    Top K Frequent Elements

---
+++ Day_12_Heaps/top_k_frequent_elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        priority_queue<pair<int,int>>pq;
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+        vector<int>ans;
+        while(k--){
+            auto it=pq.top();
+            pq.pop();
+            ans.push_back(it.second);
+        }
+        return ans; 
+    }
+};
