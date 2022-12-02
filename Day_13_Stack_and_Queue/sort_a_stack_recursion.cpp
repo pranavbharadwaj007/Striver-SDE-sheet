79b5282fcaa12a5cc02ac15c7e966184fafb1f88
Pranav N <pranavbharadwaj142@gmail.com>
2022-12-02 12:51:58 +0530

    Create sort_a_stack_recursion.cpp

---
+++ Day_13_Stack_and_Queue/sort_a_stack_recursion.cpp
@@ -0,0 +1,34 @@
+void sortedInsert(stack<int> &stack, int current)
+{
+	if (stack.empty() || current > stack.top())
+	{
+		stack.push(current);
+		return;
+	}
+
+	// Remove the top element
+	int top = stack.top();
+	stack.pop();
+	// Recursion for the remaining elements in the stack
+	sortedInsert(stack, current);
+	// Insert the popped element back in the stack
+	stack.push(top);
+}
+
+// Recursive method to sort a stack
+void sortStack(stack<int> &stack)
+{
+	// Base case: stack is empty
+	if (stack.empty())
+	{
+		return;
+	}
+
+	// Remove the top element
+	int top = stack.top();
+	stack.pop();
+	// Recursion for the remaining elements in the stack
+	sortStack(stack);
+	// Insert the popped element back in the sorted stack
+	sortedInsert(stack, top);
+}
